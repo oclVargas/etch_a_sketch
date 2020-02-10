a559c5cc551a513c7e89e58618a471f62d0f18f4
Osiel <ocl.vargasanchez@gmail.com>
2020-02-09 19:25:56 -0600

    resize board based on user input

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// clean up some of the code
+// add validation to prompt
+// change color of marker
+
+
 const board = document.querySelector('#board');
 const resetBoard = document.querySelector('.btn-reset');
+const resizeBoard = document.querySelector('.btn-resize');
+// variables for size of cell and container maybe?
+let gridContainerSize = 16;
 
 function colorChange(div) {
     div.style.backgroundColor = 'red';
@@ -12,26 +20,44 @@ function boardReset() {
     })
 }
 
+function boardResize() {
+    const cells = document.querySelectorAll('.cell')
+    let newSize = prompt('Please enter a number (the number will determine the size of the grid')
+    cells.forEach(cell => {
+        board.removeChild(cell);
+    })
+    let string = "";
 
+    for (let i = 1; i <= (newSize * newSize); i++) {
+        if (i <= newSize) {
+            string = string + " 1fr";
+            
+        }
 
-function createBoard() {
-    
-    // variables for size of cell and container maybe?
-    let cellWidth = 100 
-    let cellHeight = 100
-    let gridContainerSize = 16;
-    
+        let node = document.createElement('div');
+        node.classList.add('cell');
+        node.style.border = '1px solid black';
+        node.addEventListener('mouseover', (() => colorChange(node)))
+        board.appendChild(node)
+    }
+    board.style.gridTemplateColumns = string;
+    board.style.gridTemplateRows = string;
+    // createBoard(newSize)
+}
+
+function createBoard(gridContainerSize) {
     for (let i = 0; i < gridContainerSize; i++) {
         const div = document.createElement('div');
         div.classList.add('cell');
-        // add these styles to css instead?
-        div.style.width = `${cellWidth}px`;
-        div.style.height = `${cellHeight}px`;
         div.style.border = '1px solid black';
         div.addEventListener('mouseover', (() => colorChange(div)));
+
+        // board.style.gridTemplateColumns = `repeat(4, ${})`;
+        // board.style.gridTemplateRows = `repeat(4, ${})`;
         board.appendChild(div);
     }
 }
 
+resizeBoard.addEventListener('click', boardResize);
 resetBoard.addEventListener('click', boardReset);
-createBoard();
+createBoard(gridContainerSize);
